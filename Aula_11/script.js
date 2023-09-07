0cccc5d8c3d756a54c8252cda517d74ab7564d68
chrystian-souza <121185752+chrystian-souza@users.noreply.github.com>
2023-09-06 21:29:26 -0300

    commit

---
+++ Aula_11/script.js
@@ -0,0 +1,15 @@
+let texto = 'texto'
+let inteiro = 20
+let decimal = 20.0
+let boleano = true
+let lista = ['a','b','c',['d','3']]
+let objet = {
+    'nome' : 'Chrystian',
+    'email' : 'email@gmail.com' 
+}
+let bulo = null
+let indefinido = undefined
+
+console.log(inteiro + decimal)
+
+console.log(lista[3][0])
